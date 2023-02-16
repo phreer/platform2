4cb16b006d9c7c82239a2f9624a32c7f70246b40
Weifeng Liu <liu.wf@outlook.com>
2023-02-16 10:04:48 +0000

    vm_tools/sommelier: add Linux DMA-BUF global
    This patch implement Linux DMA-BUF protocol and expose it to clients,
    with which the clients can pass through modifier to host.

--- vm_tools/sommelier/sommelier-drm.cc
+++ vm_tools/sommelier/sommelier-drm.cc
@@ -226,7 +226,7 @@ static void sl_drm_callback_done(void* data,
   struct sl_host_drm* host =
       static_cast<sl_host_drm*>(wl_callback_get_user_data(callback));
 
-  // wl_drm_send_device(host->resource, "/dev/dri/renderD128");
+  // wl_drm_send_device(host->resource, "/dev/dri/renderD129");
   if (host->ctx->drm_device)
     wl_drm_send_device(host->resource, host->ctx->drm_device);
   if (host->version >= WL_DRM_CREATE_PRIME_BUFFER_SINCE_VERSION)
